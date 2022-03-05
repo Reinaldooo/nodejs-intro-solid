792f2d937cd8b0c04d16aad2b4c5d6acf69cba5e
Reinaldo Trindade <reinaldo.w.trindade@accenture.com>
2022-03-05 13:23:30 -0300

    Finalizando desafio

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,13 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const user_id = String(request.headers.user_id);
+      const users = this.listAllUsersUseCase.execute({ user_id });
+      return response.status(200).json(users);
+    } catch (error) {
+      return response.status(400).send({ error: error.message });
+    }
   }
 }
 
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,13 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user: User = this.usersRepository.findById(user_id);
+
+    if (!user || !user.admin) {
+      throw new Error("Invalid request");
+    }
+
+    return this.usersRepository.list();
   }
 }
 
